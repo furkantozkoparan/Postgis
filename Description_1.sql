de0a1d56804e8a0781b742cba4482dc325ea21d9
furkantozkoparan <125921593+furkantozkoparan@users.noreply.github.com>
2023-03-28 16:38:49 +0300

    Update Description_1.sql

--- Description_1.sql
+++ Description_1.sql
@@ -13,3 +13,6 @@ Select distinct(st_srid(geom)) from table_name;
 --Bir geometrinin koordinatlarını başka bir koordinat sistemine dönüştürülmesi için kullanılması gereken komut nedir? (Koordinatı 4326'ya çevirin)
 Select st_transform(geom, 4326) from table_name;
 
+--Mevcut bir tabloya geometri sütunu eklemek için kullanılması gereken komut nedir?
+SELECT AddGeometryColumn('public', 'mytable', 'geom', 4326, 'POINT', 2); 
+(Select addgeometrycolumn ('schema_name', 'table_name', 'new_column_name', 'SRID', 'geometry_type', '2D-3D'))
